c87bde433617dd14f5b8d11825ff0129f3a9332b
MarcinKucharsk1 <marcin.kucharski.stud@pw.edu.pl>
2020-11-20 18:30:48 +0000

    no chyba juz

--- lab1.cpp
+++ lab1.cpp
@@ -4,82 +4,48 @@ using namespace std;
 
 class Wektor2D
 {
-private:
-    double wspX;
-    double wspY;
-    double modul;
-
-    void norm()
-    {
-        modul = sqrt(wspX * wspX + wspY * wspY);
-        cout << " Modul wektora = " << modul << endl;
-    }
-    void print() { cout << " Wspolrzedna X: " << wspX << " Wspolrzedna Y: " << wspY << endl; }
-
-    // void num_wek **braklo mi czasu**
-
 public:
     Wektor2D()
     {
-        wspX = 1.;
-        wspY = 1.0;
-        /*out << " wektor domyslny " << endl
-             << " Wspolrzedna X: " << wspX << " Wspolrzedna Y: " << wspY << endl; */
+        wspX = 0.0;
+        wspY = 0.0;
     }
     Wektor2D(double xx, double yy)
     {
         wspX = xx;
         wspY = yy;
-        // cout << " wektor parametryczny" << endl;
-        // cout << " Wspolrzedna X: " << wspX << " Wspolrzedna Y: " << wspY << endl;
     }
+
     void setX(double aa) { wspX = aa; };
-    int  getX()
-    {
-        // cout << wspX << endl;
-        return wspX;
-    };
+    int  getX() { return wspX; };
     void setY(double aa) { wspY = aa; };
     int  getY() { return wspY; };
 
-    ~Wektor2D()
-    { /*
-         cout << " DESTRUKCJA! " << endl;
-         cout << " Nauuura wsporzedne x " << wspX << " wsppolrzedne Y " << wspY << endl;
-         */
-    }
+    friend Wektor2D operator+(Wektor2D v1, Wektor2D v2)
+    {
+        return Wektor2D(v1.getX() + v2.getX(), v1.getY() + v2.getY());
+    };
+    friend double operator*(Wektor2D v1, Wektor2D v2)
+    {
+        return v1.getX() * v2.getX() + v1.getY() * v2.getY();
+    };
+
+private:
+    double wspX;
+    double wspY;
 };
-void dodawanie_wektorow(Wektor2D v1, Wektor2D v2)
-{
-    double sumX;
-    double sumY;
 
-    sumX = v1.getX() + v2.getX();
-    sumY = v1.getY() + v2.getY();
-    cout << " Wspolrzedne wypadkowej dodawania: " << sumX << " , " << sumY << endl;
-}
-/*Wektor2D operator+(Wektor2D v1, Wektor2D v2)
-{
-    Wektor2D wyp;
-    wyp.setX(v1.getX() + v2.getX());
-    wyp.setY(v1.getY() + v2.getY());
-
-    return wyp;
-}
-double operator*(const Wektor2D v1, const Wektor2D v2)
-{
-
-    double iloczynSKAL;
-    iloczynSKAL = v1.getX() * v2.getX() + v1.getY() * v2.getY();
-    return iloczynSKAL;
-}
-*/
 int main()
 {
-    Wektor2D v1, v2;
-    v1.setX(4.);
-    v1.setY(2.);
-    v2.setX(3.);
-    v2.setY(5.);
-    // v1* v2;
+    Wektor2D v1{};         // Konstruktor domyślny, wektor o wsp. [0, 0]
+    v1.setX(1.);           // setter
+    v1.setY(1.);           // setter
+    double x1 = v1.getX(); // getter
+    double y1 = v1.getY(); // getter
+
+    Wektor2D v2{2., 2.}; // Konstruktor nadający współrzędne
+
+    Wektor2D sum = v1 + v2; // dodawanie wektorów
+
+    double prod = v1 * v2; // iloczyn skalarny
 }
--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,11 +1,10 @@
 class Wektor2D
 {
-  public:
-
-   Wektor2D()
+public:
+    Wektor2D()
     {
         wspX = 0.0;
-        wspY = 0.0;   
+        wspY = 0.0;
     }
     Wektor2D(double xx, double yy)
     {
@@ -17,17 +16,17 @@ class Wektor2D
     int  getX() { return wspX; };
     void setY(double aa) { wspY = aa; };
     int  getY() { return wspY; };
-    
-    Wektor2D operator+(Wektor2D& v1, Wektor2D& v2)
-    {
-    return Wektor2D wyp(v1.getX() + v2.getX(),v1.getY() + v2.getY());
-    }
-    double operator*(Wektor2D& v1,Wektor2D& v2)
-    {
-    return v1.getX() * v2.getX() + v1.getY() * v2.getY();}
 
-  private:
+    friend Wektor2D operator+(Wektor2D v1, Wektor2D v2)
+    {
+        return Wektor2D(v1.getX() + v2.getX(), v1.getY() + v2.getY());
+    };
+    friend double operator*(Wektor2D v1, Wektor2D v2)
+    {
+        return v1.getX() * v2.getX() + v1.getY() * v2.getY();
+    };
+
+private:
     double wspX;
     double wspY;
-
 };
